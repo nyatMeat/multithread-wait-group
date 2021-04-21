057c5e40675a92a2a7ab6967d404e6d4eb1a8b60
Alexander <nyatMeat@gmail.com>
2021-04-21 23:50:31 +0400

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+var (
+	matches   []string
+	waitgroup = sync.WaitGroup{}
+	lock      = sync.Mutex{}
+)
+
+func main() {
+
+	waitgroup.Add(1)
+	go fileSearch("C:/Users/nyatM/go", "README.md")
+	waitgroup.Wait()
+	for _, file := range matches {
+		fmt.Println("Matched", file)
+	}
+}
+
+func fileSearch(root string, filename string) {
+	fmt.Println("Searching in", root)
+	files, _ := ioutil.ReadDir(root)
+	for _, file := range files {
+		if strings.Contains(file.Name(), filename) {
+			lock.Lock()
+			matches = append(matches, filepath.Join(root, file.Name()))
+			lock.Unlock()
+		}
+		if file.IsDir() {
+			waitgroup.Add(1)
+			go fileSearch(filepath.Join(root, file.Name()), filename)
+		}
+	}
+	waitgroup.Done()
+}
